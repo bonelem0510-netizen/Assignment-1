6cfdbc9dee1ad8571dba532c8c1f8b199d5e8d8d
bonelem0510-netizen <bonelem0510@gmail.com>
2026-03-25 16:09:08 +0200

    Finished activity_main.xml

--- app/src/main/java/com/example/assignment120/MainActivity.kt
+++ app/src/main/java/com/example/assignment120/MainActivity.kt
@@ -17,31 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Assignment120Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assignment120Theme {
-        Greeting("Android")
+        //Declaration of Variables
+        val
     }
 }
