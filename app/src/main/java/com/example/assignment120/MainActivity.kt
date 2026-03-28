496728363bd2790fb060ad1a8804ca95d10ff2b0
bonelem0510-netizen <bonelem0510@gmail.com>
2026-03-28 15:48:14 +0200

    Finished activity_main.xml and MainActivity.kt

--- app/src/main/java/com/example/assignment120/MainActivity.kt
+++ app/src/main/java/com/example/assignment120/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.assignment120
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -20,6 +23,43 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         //Declaration of Variables
-        val
+        val timeInput = findViewById<EditText>(R.id.editTime)
+        val resultText = findViewById<TextView>(R.id.txtResult)
+        val checkButton = findViewById<Button>(R.id.btnCheck)
+        val resetButton = findViewById<Button>(R.id.btnReset)
+
+        //Check Button
+        checkButton.setOnClickListener {
+            val time = timeInput.text.toString()
+
+            val message = when (time){
+                "Morning" -> "Tell/Text someone Good Morning"
+                "Good Morning" -> "Send someone an uplifting message"
+                "Breakfast" -> "Send a joke to someone"
+                "Afternoon" -> "Smile and wave at someone"
+                "Good Afternoon" -> "Make conversation with someone new"
+                "Lunch" -> "Invite someone to join you for lunch"
+                "Night" -> "Ask someone about their day"
+                "Goodnight" -> "Show gratitude towards someone"
+                "Dinner" -> "Share a fun memory from your day"
+                "Done" -> "Amazing Work, Well done"
+                else -> "Answer not accepted"
+            }
+
+            resultText.text = message
+
+        }
+        //Reset Button
+        resetButton.setOnClickListener {
+            timeInput.text.clear()
+            resultText.text = "Result will appear here"
+        }
+
     }
 }
+
+
+
+
+
+
